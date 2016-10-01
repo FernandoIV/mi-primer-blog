1e1f5f27af7d65345060b0f17c79c53d68625d91
InfectedBoar <pain.15592@gmail.com>
2016-09-30 20:05:32 -0500

    Formularios para crear post nuevo y editar post

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ app_name = 'blog'
 urlpatterns = [
 	url(r'^$',views.post_list),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/nuevo/$',views.post_nuevo,name='post_nuevo'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_editar, name='post_editar'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
@@ -9,3 +10,29 @@ def post_list(request):
 def post_detail(request, pk):
         post = get_object_or_404(Post, pk=pk)
         return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_nuevo(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.autor = request.user
+			post.fecha_publicacion = timezone.now()
+			post.save()
+			return redirect('blog:post_detail',pk=post.pk)
+	else:
+		form = PostForm()
+	return render(request,'blog/post_editar.html',{'form':form})
+
+def post_editar(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit = False)
+			post.autor = request.user
+			post.save()
+			return redirect('blog:post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'blog/post_editar.html',{'form':form})
